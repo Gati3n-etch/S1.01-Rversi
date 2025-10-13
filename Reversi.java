a442b4a80e57a094b49174a693c3d919b7bece1b
Bigno0bz <guebhart.e2501197@etud.univ-ubs.fr>
2025-10-13 14:05:51 +0200

    Version 2

--- Reversi.java
+++ Reversi.java
@@ -1,6 +1,6 @@
 class Reversi{
 	void principal(){
-		String[][] t = tableau();
+		char[][] t = tableau();
 		displayTab(t);
 	}
 	
@@ -13,39 +13,78 @@ class Reversi{
 		}
 		return modeJeu;
 	}
-	String[][] tableau(){
+	char[][] tableau(){
 		int taille=SimpleInput.getInt("Entrez la taille du tableau : ");
 		while (taille%2!=0 || taille<4 || taille>16){
 			System.out.println("La valeur n'est pas valide");
 			taille=SimpleInput.getInt("Entrez la taille du tableau : ");
 		}
-		String[][] plateau =new String[taille][taille];
+		char[][] plateau =new char[taille][taille];
 		for (int i = 0; i < taille; i++) {
 			for (int j = 0; j < taille; j++) {
-				plateau[i][j] = " ";
+				plateau[i][j] = ' ';
 			}
 		}
 		int milieu = taille/2;
-        plateau[milieu][milieu] = "O";
-        plateau[milieu-1][milieu-1] = "O";
-        plateau[milieu-1][milieu] = "X";
-        plateau[milieu][milieu-1] = "X";
+        plateau[milieu][milieu] = 'O';
+        plateau[milieu-1][milieu-1] = 'O';
+        plateau[milieu-1][milieu] = 'X';
+        plateau[milieu][milieu-1] = 'X';
 		return plateau;
 	}
 	
 	
-	int[] listeVoisin(int x; int y; String tab){
-		int[] voisin = new int[];
-		if(tab[y][x] !=
+	void tableauNum(char[][] tab){
+	}
+	
+	
+	int[][] listeVoisin(int x, int y, char[][] tab){
+		int[][] voisin = new int[4][2];
+		if(tab[y][x-1] != ' ' && x-1 >= 0){
+			voisin[0][0] = x;
+			voisin[0][1] = y;
+		}
+		else if(tab[y][x-1] == ' ' && x-1 >= 0){
+			voisin[0][0] = -1;
+			voisin[0][1] = -1;
+		}
+		else if(tab[y][x+1] != ' ' && x+1 < tab.length){
+			voisin[1][0] = x;
+			voisin[1][1] = y;
+		}
+		else if(tab[y][x+1] == ' ' && x+1 < tab.length ){
+			voisin[1][0] = -1;
+			voisin[1][1] = -1;
+		}
+		else if(tab[y-1][x] != ' ' && y-1 >= 0){
+			voisin[1][0] = x;
+			voisin[1][1] = y;
+		}
+		else if(tab[y-1][x] == ' ' && y-1 >= 0){
+			voisin[2][0] = -1;
+			voisin[2][1] = -1;
+		}
+		else if(tab[y+1][x] != ' ' && y+1 < tab.length){
+			voisin[3][0] = x;
+			voisin[3][1] = y;
+		}
+		else if(tab[y+1][x] == ' ' && y+1 < tab.length ){
+			voisin[3][0] = -1;
+			voisin[3][1] = -1;
+		}
+		
+		return voisin;
+	}
+			
 		
 		
 	
-	void changementCase(int x; int y; String tab; String val){
+	void changementCase(int x, int y, char[][] tab, char val){
 		tab[y][x] = val;
 	}
 	
 	
-	void displayTab(String[][] t){
+	void displayTab(char[][] t){
 		String ligne_haut = "    ";
 		for(int i = 0; i < t.length ; i++){
 			if(i>=10){
