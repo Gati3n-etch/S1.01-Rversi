d506c7d4cad673534bd73b8f3e51dd33ea0aa161
Bigno0bz <guebhart.e2501197@etud.univ-ubs.fr>
2025-10-17 10:52:15 +0200

    Version 17/10/25

--- Reversi.java
+++ Reversi.java
@@ -4,8 +4,26 @@ class Reversi{
 		displayTab(t);
 	}
 	
+	
+	void lancementJeu(){
+		int mode = choixMode();
+		char[][] plateauJeu = tableau();
+		displayTab(plateauJeu);
+		int[] couleurPiontJoueur = choixJoueur(mode);
+		int joueur1 = couleurPiontJoueur[0];
+		int joueur2 = couleurPiontJoueur[1];
+		/* Mode a 2 */
+		if(mode==2){
+			
+		}
+		/* Mode solo */
+		else{
+		
+		}
+	}
 
-	int choix_mode(){
+
+	int choixMode(){
 		int modeJeu=SimpleInput.getInt("Choisissez le mode jeu entre solo (1) et duo (2) : ");
 		while (modeJeu != 1 && modeJeu != 2){
 			System.out.println("la valeur n'est pas valide");
@@ -13,6 +31,41 @@ class Reversi{
 		}
 		return modeJeu;
 	}
+	
+	
+	int[] choixJoueur(int mode){
+		int joueur1 = SimpleInput.getChar("Joueur 1 choisissez la couleur des pionts [ blanc (1) ou noir (2) ] : ");
+		int joueur2 = 2;
+		while(joueur1 != 1 && joueur1 != 2){
+			System.out.println("Valeur invalide !");
+			joueur1 = SimpleInput.getChar("Joueur 1 choisissez la couleur des pionts [ blanc (1) ou noir (2) ] : ");
+		}
+		if(mode==2){
+			if(joueur1 == 1){
+				System.out.println("Joueur 2 aura les pionts de couleurs noirs");
+				joueur2 = 2;
+			}
+			else{
+				System.out.println("Joueur 2 aura les pionts de couleurs blancs");
+				joueur2 = 1;
+			}
+		}
+		else{
+			if(joueur1 == 1){
+				System.out.println("Le robot aura les pionts de couleurs noirs");
+				joueur2 = 2;
+			}
+			else{
+				System.out.println("Le robot aura les pionts de couleurs blancs");
+				joueur2 = 1;
+			}
+		}
+		int[] piontJoueur = {joueur1,joueur2};
+		return piontJoueur;
+	}
+	
+	
+	
 	char[][] tableau(){
 		int taille=SimpleInput.getInt("Entrez la taille du tableau : ");
 		while (taille%2!=0 || taille<4 || taille>16){
@@ -34,34 +87,43 @@ class Reversi{
 	}
 	
 	
+	
 	void coupJouer(char[][] tab, int joueur){
-		char signe;
+		char signe = 'X';
 		if(joueur == 1){
 			signe = 'O';
-		}else{
-			signe = 'X';
 		}
 		int x = SimpleInput.getInt("Choisissez la colone : ");
+		while(x<0 && x>=tab.length){
+			System.out.println("Valeur saisi en dehors des limites du plateau");
+			x = SimpleInput.getInt("Choisissez la colone : ");
+		}
 		int y = SimpleInput.getInt("Choisissez la ligne : ");
+		while(y<0 && y>=tab.length){
+			System.out.println("Valeur saisi en dehors des limites du plateau");
+			y = SimpleInput.getInt("Choisissez la ligne : ");
+		}
 		while(estJouable(x,y,tab) == false){
 			System.out.println("Ce coup ne peut pas etre jouer choisissez une autre case.");
 			x = SimpleInput.getInt("Choisissez la colone : ");
 			y = SimpleInput.getInt("Choisissez la ligne : ");
 		}
-		tab[y][x] = signe;
+		changementCase(x,y,tab,signe);
 		/**
 		 * Appel fonction pour remplacer les cases
 		*/
+		displayTab(tab);
 	}
+	
 		
 	
 	boolean estJouable(int x, int y, char[][] tab){
-		boolean jouable = true;
-		if(tab[y][x] == ' '){
+		boolean jouable = false;
+		if(y>=0 && y<tab.length && x>=0 && x<tab.length && tab[y][x] == ' '){
 			int[][] voisins = listeVoisin(x, y, tab);
 			for(int i = 0; i<voisins.length; i++){
-				if(voisins[i][0] == -1){
-					jouable = false;
+				if(voisins[i][0] != -1){
+					jouable = true;
 				}
 			}
 		}else{
@@ -71,50 +133,46 @@ class Reversi{
 	}
 	
 	
+	
 	int[][] listeVoisin(int x, int y, char[][] tab){
 		int[][] voisin = new int[4][2];
-		if(tab[y][x-1] != ' ' && x-1 >= 0){
+		if(x-1 >= 0 && tab[y][x-1] != ' '){
 			voisin[0][0] = x;
 			voisin[0][1] = y;
-		}
-		else if(tab[y][x-1] == ' ' && x-1 >= 0){
+		}else{
 			voisin[0][0] = -1;
 			voisin[0][1] = -1;
 		}
-		else if(tab[y][x+1] != ' ' && x+1 < tab.length){
+		if(x+1 < tab.length && tab[y][x+1] != ' '){
 			voisin[1][0] = x;
 			voisin[1][1] = y;
-		}
-		else if(tab[y][x+1] == ' ' && x+1 < tab.length ){
+		}else{
 			voisin[1][0] = -1;
 			voisin[1][1] = -1;
 		}
-		else if(tab[y-1][x] != ' ' && y-1 >= 0){
+		if(y-1 >= 0 && tab[y-1][x] != ' '){
 			voisin[1][0] = x;
 			voisin[1][1] = y;
-		}
-		else if(tab[y-1][x] == ' ' && y-1 >= 0){
+		}else{
 			voisin[2][0] = -1;
 			voisin[2][1] = -1;
 		}
-		else if(tab[y+1][x] != ' ' && y+1 < tab.length){
+		if(y+1 < tab.length && tab[y+1][x] != ' '){
 			voisin[3][0] = x;
 			voisin[3][1] = y;
-		}
-		else if(tab[y+1][x] == ' ' && y+1 < tab.length ){
+		}else{
 			voisin[3][0] = -1;
 			voisin[3][1] = -1;
 		}
-		
 		return voisin;
 	}
 			
 		
 		
-	
 	void changementCase(int x, int y, char[][] tab, char val){
 		tab[y][x] = val;
 	}
+	
 	
 	
 	void displayTab(char[][] t){
@@ -140,7 +198,11 @@ class Reversi{
 				System.out.print(y+" ");
 			}
 			for(int x = 0; x < t.length ; x++){
-				System.out.print("| "+t[y][x]+" ");
+				if(estJouable(x,y,t)){
+					System.out.print("| ~ ");
+				}else{
+					System.out.print("| "+t[y][x]+" ");
+				}
 			}
 			System.out.println("|");
 		}
@@ -148,6 +210,8 @@ class Reversi{
 		for(int a = 0; a < t.length ; a++){
 			System.out.print("----");
 		}
+		System.out.println();
+		System.out.println("O : piont blanc / X : piont noir / ~ : case valide");
 		
 	}			
 }
